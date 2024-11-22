0246b89bbbd9b8012dba88bf742236294b6eb949
majidious <onaisym4@gmail.com>
2024-11-22 19:52:22 +0200

    Initial Commit

---
+++ backend/Data/AppDataContext.cs
@@ -0,0 +1,12 @@
+﻿namespace backend.Data
+{
+    using backend.Models;
+    using Microsoft.EntityFrameworkCore;
+    public class AppDataContext : DbContext
+    {
+        public AppDataContext(DbContextOptions<AppDataContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
